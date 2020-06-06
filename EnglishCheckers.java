96c96a62db13ed5fc3d8651064a4f587df2f4e7e
meir shomron <meirshomron@gmail.com>
2020-06-06 16:01:15 +0300

    Update EnglishCheckers.java

--- EnglishCheckers.java
+++ EnglishCheckers.java
@@ -9,7 +9,7 @@ public class EnglishCheckers
 	public static final int BLUE = -1;
 	public static final int EMPTY = 0;
 
-	public static final int BOARD_SIZE = 12;
+	public static final int BOARD_SIZE = 8;
 	public static final int MARK = 3;
 	
 	public static EnglishCheckersGUI grid;
